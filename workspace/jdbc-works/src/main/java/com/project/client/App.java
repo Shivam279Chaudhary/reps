53c5603233b901d01dd0047c225fd438839c8ad4
Akhil <akhiltomar098@gmail.com>
2021-05-04 17:25:38 +0530

    Answer DAO with basic fuctionalities

--- workspace/jdbc-works/src/main/java/com/project/client/App.java
+++ workspace/jdbc-works/src/main/java/com/project/client/App.java
@@ -1,34 +1,57 @@
 package com.project.client;
 
+import com.project.dao.AnswerDAO;
 import com.project.dao.QuestionDOO;
+import com.project.entity.Answer;
 import com.project.entity.Question;
+import com.project.interfaces.IAnswerDAO;
 import com.project.interfaces.IQuestionDAO;
 
 public class App {
 	public static void main(String[] args) {
 		
-		Question question = new Question(); 
-		question.setQuestionID(12);
-		question.setTitle("How can i be fit forever?");
-		question.setDescription("dont simply sit work out to burn calories"); 
-		question.setVote(3);
-		question.setCategoryID(1);
-		question.setUserID(1);
-		question.setImageLink("https://something.png");
-		question.setReliability(10);
+//		Question question = new Question(); 
+//		question.setQuestionID(12);
+//		question.setTitle("How can i be fit forever?");
+//		question.setDescription("dont simply sit work out to burn calories"); 
+//		question.setVote(3);
+//		question.setCategoryID(1);
+//		question.setUserID(1);
+//		question.setImageLink("https://something.png");
+//		question.setReliability(10);
 		java.util.Date dt = new java.util.Date();
 		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String currentTime = sdf.format(dt);
-		question.setModifiedAt(currentTime);
+//		question.setModifiedAt(currentTime);
+//		
+//		IQuestionDAO dao = new QuestionDOO(); 
 		
-		IQuestionDAO dao = new QuestionDOO(); 
+//		System.out.println(dao.insertQuestion(question) ?"Inserted":"Not Inserted");
+//		
+//		for(Question q : dao.getAllQuestion()) {
+//			System.out.println(q);
+//		}
+//		
 		
-		System.out.println(dao.insertQuestion(question) ?"Inserted":"Not Inserted");
+		Answer answer = new Answer();
+		answer.setAnswerID(10);
+		answer.setDescription("  updated new Answer");
+		answer.setVotes(10);
+		answer.setModifiedAt(currentTime);
+		answer.setQuestionID(2);
+		answer.setUserID(3);
+		answer.setReliability(10);
 		
-		for(Question q : dao.getAllQuestion()) {
-			System.out.println(q);
-		}
+		IAnswerDAO answerDAO = new AnswerDAO();
 		
+//		System.out.println(answerDAO.insertAnswer(answer) ?"Inserted":"Not Inserted");
+		
+		answerDAO.updateAnswerByAnswerID(answer);
+		
+		
+//		for(Answer ans : answerDAO.getAllAnswersASC(2)) {
+//			System.out.println(ans);
+//		}
 		
 	}
 }
